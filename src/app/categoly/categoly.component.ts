fb0b4163d40ba508644e1ebbf7319fc18218ebf9
Vuthuyhangbamboo <hangvtph07414@fpt.edu.vn>
2020-03-14 09:03:35 +0700

    Initial commit

---
+++ src/app/categoly/categoly.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categoly',
+  templateUrl: './categoly.component.html',
+  styleUrls: ['./categoly.component.css']
+})
+export class CategolyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
