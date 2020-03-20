3fccd56aadfd9a7fdaeb0a24e02132ed78b39e9c
Vuthuyhangbamboo <hangvtph07414@fpt.edu.vn>
2020-03-20 15:48:37 +0700

    1 file modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,13 @@ import { SlideComponent } from './slide/slide.component';
 import { CategolyComponent } from './categoly/categoly.component';
 import { TableComponent } from './table/table.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductListComponent, ProductComponent, SlideComponent, CategolyComponent, TableComponent, ProductDetailComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductListComponent, ProductComponent, SlideComponent, CategolyComponent, TableComponent, ProductDetailComponent, AddProductComponent, AboutComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
