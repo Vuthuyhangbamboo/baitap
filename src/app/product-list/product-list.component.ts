26787fd168eaf9fbfb3b569a3fa1afb06df00f60
Vuthuyhangbamboo <hangvtph07414@fpt.edu.vn>
2020-03-25 22:11:11 +0700

    3 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../product';
-import {data} from "../mock";
-
+import { Product } from '../Product';
+import { ProductService } from '../services/add-product.service'
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  products: Product[];
+  constructor(
+    private productService: ProductService
+  ) { }
 
-  constructor() { }
+  ngOnInit(): void {
+    this.getProduct(id);
+  }
 
-  ngOnInit() {
+
+  getProduct(id){
+    return this.products.find(product => product.id == id);
   }
-  products = data;
-  selected: Product;
-  showDetail(product){
-    this.selected = product;
-  }
+  
+  // products;
+  // selected: Product;
+  // showDetail(product){
+  //   this.selected = product;
+  //   console.log(this.selected);
+  // }
   removeItem(id){
-    return this.products = this.products.filter(product => product.id != id);
+    this.products = this.productService.removeProduct(id);
+    // this.products = this.products.filter(product => product.id != id)
   }
 }
